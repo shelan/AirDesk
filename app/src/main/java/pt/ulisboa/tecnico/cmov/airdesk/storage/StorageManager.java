cb1f8c0632640b6b66d0dd2a367e6e55e1f10b17
Chathuri Gunawardhana <lanch.gunawardhana@gmail.com>
2015-03-15 22:14:48 +0000

    Bug fix:- added application context for accessing files, removed unused code

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/storage/StorageManager.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/storage/StorageManager.java
@@ -27,63 +27,6 @@ public class StorageManager {
     private String workspaceDir = Constants.WS_DIR;
     private String foreignWorkspaceDir = Constants.WS_DIR;
 
-    public String readFromInternalFile()
-    {
-        FileInputStream fis = null;
-        try {
-            Context appContext = AirDeskApp.s_applicationContext;
-            fis = appContext.openFileInput("user.jons");
-            String jsonString = readStreamAsString(fis);
-            return jsonString;
-        }
-        catch(IOException x)
-        {
-            return null;
-        }
-
-    }
-
-    private String readStreamAsString(InputStream is)
-            throws FileNotFoundException, IOException {
-        ByteArrayOutputStream baos = null;
-        try {
-            baos = new ByteArrayOutputStream();
-            copy(is, baos);
-            baos.close();
-            return baos.toString();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-    private void copy(InputStream reader, OutputStream writer)
-            throws IOException
-    {
-        byte byteArray[] = new byte[4092];
-        while(true) {
-            int numOfBytesRead = reader.read(byteArray,0,4092);
-            if (numOfBytesRead == -1) {
-                break;
-            }
-            writer.write(byteArray,0,numOfBytesRead);
-        }
-        return;
-    }
-    public void saveToInternalFile(String ins)
-    {
-        FileOutputStream fos = null;
-        try {
-            Context appContext = AirDeskApp.s_applicationContext;
-            fos = appContext.openFileOutput("user.jons"
-                    ,Context.MODE_PRIVATE);
-            fos.write(ins.getBytes());
-            fos.close();
-        }
-        catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
-
     public boolean createDataFile(String workspaceName, String fileName) throws IOException {
         String path =  workspaceDir+ workspaceName + "/" + fileName;
         return new File(path).createNewFile();
