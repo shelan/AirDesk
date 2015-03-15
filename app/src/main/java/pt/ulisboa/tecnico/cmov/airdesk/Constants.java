fa4a1b56c0ef9788914df44382362db78fc273cf
ashansa <ashansa.perera@gmail.com>
2015-03-15 09:19:42 +0000

    adding Constants

---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/Constants.java
@@ -0,0 +1,9 @@
+package pt.ulisboa.tecnico.cmov.airdesk;
+
+/**
+ * Created by ashansa on 3/15/15.
+ */
+public class Constants {
+
+    public static final String WS_DIR = "/Users/ashansa/softwares/github/uni_projects/2semester/AirDesk/app";
+}
