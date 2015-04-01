19e019fa7dc0123033ed4359be3f3feeb7b341d6
shelan <shelanrc@gmail.com>
2015-04-01 20:45:28 +0100

    Adding Text file edit

--- app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/activity/TextFileEditActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/activity/TextFileEditActivity.java
@@ -1,17 +1,22 @@
 package pt.ulisboa.tecnico.cmov.airdesk.activity;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -28,7 +33,9 @@ import pt.ulisboa.tecnico.cmov.airdesk.manager.WorkspaceManager;
 
 public class TextFileEditActivity extends ActionBarActivity {
 
-    static TextView fileText;
+    static EditText fileText;
+    static TextFile file;
+    static Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,7 @@ public class TextFileEditActivity extends ActionBarActivity {
         return true;
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -58,6 +66,14 @@ public class TextFileEditActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+
+        if (id == R.id.action_edit_file) {
+
+            saveButton.setVisibility(View.VISIBLE);
+            fileText.setBackground(null);
+            fileText.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+            Toast.makeText(this, "Editing", Toast.LENGTH_SHORT).show();
         }
 
         return super.onOptionsItemSelected(item);
@@ -76,11 +92,12 @@ public class TextFileEditActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_text_file_edit, container, false);
-            fileText = (TextView) rootView.findViewById(R.id.file_text);
+            final View rootView = inflater.inflate(R.layout.fragment_text_file_edit, container, false);
+            fileText = (EditText) rootView.findViewById(R.id.file_text);
+            fileText.setInputType(InputType.TYPE_NULL);
 
             Intent intent = getActivity().getIntent();
-            TextFile file = new TextFile();
+            file = new TextFile();
 
             if (intent != null) {
 
@@ -98,10 +115,28 @@ public class TextFileEditActivity extends ActionBarActivity {
             try {
                 setFileText(file);
             } catch (ExecutionException e) {
-                Log.d(LOG_TAG,"Error while executing file get Async task");
+                Log.d(LOG_TAG, "Error while executing file get Async task");
             } catch (InterruptedException e) {
-                Log.d(LOG_TAG,"Interrupted while executing file get Async task");
+                Log.d(LOG_TAG, "Interrupted while executing file get Async task");
             }
+
+            saveButton = (Button) rootView.findViewById(R.id.file_save_btn);
+            saveButton.setVisibility(View.INVISIBLE);
+            saveButton.setOnClickListener(new Button.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    try {
+                        saveFileText(file);
+                        saveButton.setVisibility(View.INVISIBLE);
+                        getActivity().finish();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
             return rootView;
         }
@@ -110,6 +145,13 @@ public class TextFileEditActivity extends ActionBarActivity {
             FileStreamAsyncTask fileStreamAsyncTask = new FileStreamAsyncTask();
             fileStreamAsyncTask.execute(file);
         }
+
+
+        private void saveFileText(TextFile file) throws ExecutionException, InterruptedException {
+            FileSaveAsynTask fileSaveAsynTask = new FileSaveAsynTask();
+            fileSaveAsynTask.execute(file);
+        }
+
     }
 
 
@@ -159,5 +201,33 @@ public class TextFileEditActivity extends ActionBarActivity {
             }
             fileText.setText(textBuffer);
         }
+    }
+
+    public static class FileSaveAsynTask extends AsyncTask<TextFile, Void, Boolean> {
+
+        private final String LOG_TAG = FileSaveAsynTask.class.getSimpleName();
+
+        WorkspaceManager manager = new WorkspaceManager();
+
+        @Override
+        protected Boolean doInBackground(TextFile... params) {
+            TextFile[] textFileList = params;
+
+            if (textFileList != null && textFileList.length == 1) {
+                TextFile textFile = textFileList[0];
+
+                //TODO:Change this accordingly to pass isOwner parameter using and intent
+                try {
+                     manager.updateDataFile(textFile.getWorkspace(), textFile.getFileName(), fileText.getText().toString(),
+                            textFile.getOwner()
+                            , true);
+                    return true;
+                } catch (IOException e) {
+                    Log.d(LOG_TAG, "Error while saving the file");
+                }
+            }
+            return null;
+        }
+
     }
 }
